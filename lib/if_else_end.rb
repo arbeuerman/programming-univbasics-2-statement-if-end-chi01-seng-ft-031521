3bed1d315bc258c107d2d9969f2e47a1a975ca1a
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-09 20:40:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -13,3 +13,11 @@ end
 puts "Do you know the year?"
 this_year = Time.now.year
 puts "Nope, it's not 2021" unless this_year == 2020
+
+current_second = Time.now.select_second
+if current_second % 2 == 0
+  puts "Even!"
+else
+  "Odd!"
+end 
+  
